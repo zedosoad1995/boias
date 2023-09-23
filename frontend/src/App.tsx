58a195a81b13ca694894a657e939c7e2d4c40fd4
João Queiroz <joao.pires.queiroz@gmail.com>
2023-09-23 01:39:28 +0100

    Add filters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,11 @@ import {
   Legend,
 } from "chart.js";
 import { getBuoysData } from "./Services";
+import { downloadExcel, filterByDate, subsample } from "./Utils";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const colors = ["rgba(93, 120, 255, 1)", "rgba(255, 87, 87, 1)"];
 
 const categoryLabels = {
   height: ["HMAX", "HS"],
@@ -22,62 +25,131 @@ const categoryLabels = {
 } as Record<string, string[]>;
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [range, setRange] = useState("today");
   const [category, setCategory] = useState("temperature");
   const [data, setData] = useState({
-    labels: [],
+    labels: [] as string[],
     datasets: [
       {
         label: "Temperature",
-        data: [],
+        data: [] as any[],
         borderColor: "rgba(75,192,192,1)",
-        backgroundColor: "rgba(75,192,192,0.4)",
-        tension: 0.5,
+        backgroundColor: "rgba(75,192,192,1)",
+        tension: 0.4,
       },
     ],
   });
   const [rows, setRows] = useState<{ [k: string]: any }>({});
 
   useEffect(() => {
-    getBuoysData().then((record) => setRows(record));
+    getBuoysData()
+      .then((record) => setRows(record))
+      .finally(() => setIsLoading(false));
   }, []);
 
   useEffect(() => {
     if (!rows[category]) return;
 
     const labelNames = categoryLabels[category];
-    const labels = rows[category].map((row: any) => row.SDATA);
+
+    const categoryData = filterByDate(rows[category], range);
+
+    const labels = subsample(categoryData.map((row: any) => row.SDATA));
 
     setData((data) => {
       const datasets = labelNames.map((label, index) => ({
         ...data.datasets[index],
+        borderColor: colors[index],
+        backgroundColor: colors[index],
         label,
-        data: rows[category].map((row: any) => row[label]),
+        data: subsample(categoryData.map((row: any) => row[label])),
+        tension: 0.4,
+        cubicInterpolationMode: "monotone",
       }));
 
       return {
         ...data,
-        labels,
+        labels: labels,
         datasets,
       };
     });
-  }, [rows, category]);
+  }, [rows, category, range]);
 
   const handleChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setCategory(event.currentTarget.value);
   };
+  const handleChangeRange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRange(event.currentTarget.value);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleDownload = () => {
+    downloadExcel([
+      { sheetName: "height", data: rows.height },
+      { sheetName: "period", data: rows.period },
+      { sheetName: "direction", data: rows.direction },
+      { sheetName: "temperature", data: rows.temperature },
+    ]);
+  };
 
   return (
-    <div>
-      <h2>Line Chart Example</h2>
-      <select name="Categorias" value={category} onChange={handleChangeCategory}>
-        <option value="height">Altura</option>
-        <option value="period">Período</option>
-        <option value="direction">Direção</option>
-        <option value="temperature">Temperatura</option>
-      </select>
-      <div style={{ height: "400px", width: "600px" }}>
-        <Line data={data} />
+    <div className="container">
+      <h2>Boías do Algarve</h2>
+      <div style={{ display: "flex", gap: 8 }}>
+        <select name="Categorias" value={category} onChange={handleChangeCategory}>
+          <option value="height">Altura</option>
+          <option value="period">Período</option>
+          <option value="direction">Direção</option>
+          <option value="temperature">Temperatura</option>
+        </select>
+        <select name="Range" value={range} onChange={handleChangeRange}>
+          <option value="today">Hoje</option>
+          <option value="last_week">Semana</option>
+          <option value="last_month">Mês</option>
+          <option value="last_year">Ano</option>
+          <option value="total">Total</option>
+        </select>
+        <button onClick={handleDownload}>Download</button>
       </div>
+      {isLoading && <div style={{ marginTop: 8 }}>A carregar...</div>}
+      {!isLoading && (
+        <div style={{ width: "100%", overflowX: isMobile ? "auto" : undefined }}>
+          <div
+            style={{
+              height: "70vh",
+              width: isMobile ? 700 : "",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Line
+              data={data}
+              options={{
+                responsive: true,
+                maintainAspectRatio: true,
+                interaction: {
+                  mode: "index",
+                  intersect: false,
+                },
+              }}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ frontend/src/Utils.ts
@@ -0,0 +1,56 @@
+import { saveAs } from "file-saver";
+import * as XLSX from "xlsx";
+
+interface DataObject {
+  sheetName: string;
+  data: any[];
+}
+
+export const downloadExcel = (multiData: DataObject[]) => {
+  const wb: XLSX.WorkBook = XLSX.utils.book_new();
+
+  multiData.forEach((dataObj, index) => {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataObj.data);
+    XLSX.utils.book_append_sheet(wb, ws, dataObj.sheetName || `Sheet${index + 1}`);
+  });
+
+  const excelBuffer: any = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+  const blob: Blob = new Blob([excelBuffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+  });
+  saveAs(blob, "boias-do-algarve.xlsx");
+};
+
+export const subsample = <T = any>(data: T[], maxSize = 100) => {
+  const sampleFactor = Math.ceil(data.length / maxSize);
+
+  return data.filter((_, index) => index % sampleFactor === 0);
+};
+
+export const filterByDate = (array: any[], option: string) => {
+  const now = new Date();
+  const nowTime = now.getTime();
+  let startDate: Date;
+
+  switch (option) {
+    case "today":
+      startDate = new Date(nowTime - 1 * 24 * 60 * 60 * 1000);
+      break;
+    case "last_week":
+      startDate = new Date(nowTime - 7 * 24 * 60 * 60 * 1000);
+      break;
+    case "last_month":
+      startDate = new Date(nowTime - 30 * 24 * 60 * 60 * 1000);
+      break;
+    case "last_year":
+      startDate = new Date(nowTime - 365 * 24 * 60 * 60 * 1000);
+      break;
+    case "total":
+      return array;
+  }
+
+  return array.filter((item) => {
+    const itemDate = new Date(item.SDATA);
+    return itemDate >= startDate && itemDate <= now;
+  });
+};
